dbf39ccf50a3d2815d7b29f6ee1aefac8b1fdd29
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-11-03 22:33:53 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -1,10 +1,10 @@
 print("'Python' is not recognized")
 c,p,s=input(),0,[]
 while len(c)>p:
- print(c[p])
  if c[p]==";":break
  elif c[p]==":":s.append(c[p+1]);p+=1
- elif c[p]=="\\":print(c[p+1],end="");p+=1
+ elif c[p]=="\\":
+  while c[p+1]!="/":print(c[p+1],end="");p+=1
  elif c[p]=="?"and c[p+1]=="[":
   if s[0]==s[1]:
    while c[p]!="]":p+=1
