87bbb713ddd5af45e2b8e94fcbe5c499bcec2a69
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-11-03 14:31:29 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -3,6 +3,5 @@ c,p,v=input(),0,{"n":[],"v":[]}
 while len(c)>p:
  if c[p]==";":break
  elif c[p]==":":v["n"].append(c[p+1]);v["v"].append(c[p+2]);p+=2
- elif c[p]=="\\":print(c[p+1]);p+=1
+ elif c[p]=="\\":print(c[p+1],end="");p+=1
  p+=1
-print(p,"bytes")
