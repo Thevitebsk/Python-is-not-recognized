1414ad0815e6c2f34f15e1d05b42a422727f68b7
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-11-03 14:30:26 +0300

    interpriter

---
+++ 'P'inr.py
@@ -0,0 +1,8 @@
+print("'Python' is not recognized")
+c,p,v=input(),0,{"n":[],"v":[]}
+while len(c)>p:
+ if c[p]==";":break
+ elif c[p]==":":v["n"].append(c[p+1]);v["v"].append(c[p+2]);p+=2
+ elif c[p]=="\\":print(c[p+1]);p+=1
+ p+=1
+print(p,"bytes")
