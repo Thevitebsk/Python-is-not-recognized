f14317b5b6359b913a6c7050a9e5fb0a9aaf8a7b
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-11-04 22:23:43 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -4,7 +4,7 @@ for ic in range(pl):c.append(input(f"{n}:"));n+=1
 mc=c[0];p=0
 while len(c)>0:
  while len(mc)>p:
-  if mc[p]=="t":print(c[p-1]);p+=1
-  if mc[p]==";":break
+  if mc[p]=="t" and len(c)>1:print(c[p+1]);p+=1
+  elif mc[p]==";":break
   p+=1
  c.pop(0)
