940bf949ff02b3e8d6d8cb5079608eb6329a041a
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-11-04 22:12:09 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -1,12 +1,10 @@
 print("'Python' is not recognized")
-c,p,s=input(),0,[]
-while len(c)>p:
- if c[p]==";":break
- elif c[p]==":":s.append(c[p+1]);p+=1
- elif c[p]=="?"and c[p+1]=="[":
-  if s[0]==s[1]:None
-  else:
-   while c[p]!="]":p+=1
- elif c[p]=="@":s.append(input())
- elif c[p]=="|":print(c[p+1],end="");p+=2
- p+=1
+pl=int(input("Amout of lines:"));c=[];n=1;mc=""
+for ic in range(pl):c.append(input(f"{n}:"));n+=1
+mc=c[0];p=0
+while len(c)>0:
+ while len(mc)>p:
+  if mc[p]=="t":print(c[p-1]);p+=1
+  if mc[p]==";":break
+  p+=1
+ c.pop(0)
