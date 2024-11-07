14d3b65347ab047daa278625ce364b0e778dc5ff
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-07 11:13:01 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -1,13 +1,12 @@
 print("'Python' is not recognized")
-pl=int(input("Amout of lines:"));c=[];mc="";cp=0;print(pl);sv=[]
-for ic in range(pl):c.append(input())
-mc=c[0];p=0;s=0
-while len(c)>0:
- while len(mc)>p:
-  if mc[p]=="t":print(c[int(mc[p+1])]);p+=1
-  elif mc[p]==";":s=1;break
-  elif mc[p]=="g":mc=c[p];p=-1
-  p+=1
- if s==1:break
- sv.append(c.pop(0))
- if len(c)>0:mc=c[0];p=0
+c,p,s=input(),0,[]
+while len(c)>p:
+ if c[p]==";":break
+ elif c[p]==":":s.append(c[p+1]);p+=1
+ elif c[p]=="?"and c[p+1]=="[":
+  if s[0]==s[1]:None
+  else:
+   while c[p]!="]":p+=1
+ elif c[p]=="@":s.append(input())
+ elif c[p]=="|":print(c[p+1],end="");p+=2
+ p+=1
