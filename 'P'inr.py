b449bf6b112ec0e8b994cc695a6ccf525bb5e0f8
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-07 22:39:47 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -4,7 +4,7 @@ while len(c)>p:
  if c[p]==";":break
  elif c[p]==":":s.append(c[p+1]);p+=1
  elif c[p]=="?"and c[p+1]=="[":
-  if s[0]==s[1]:None
+  if s[0]==s[1]:...
   else:
    while c[p]!="]":p+=1
  elif c[p]=="@":s.append(input())
