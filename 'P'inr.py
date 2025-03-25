d9d5c19c78e3df4548ba39253775211202b6d566
Gaham <glebsemenov1mail.ru@gmail.com>
2025-03-25 22:05:06 +0300

    Update 'P'inr.py

--- 'P'inr.py
+++ 'P'inr.py
@@ -7,11 +7,12 @@ while len(c)>p:
   if s[-1]!=s[-2]:p=c.index("]",p)
  elif c[p]=="@":s.append(input())
  elif c[p]=="|":print(c[p+1],end="");p+=1
- if c[p]=="j":
+ elif c[p]=="j":
   trla=str(c[p+1]);p+=2
   while 1:
    if len(c)==p:p=0
    elif c[p]==trla and c[p-1]==":":break
    p+=1
- if c[p]=="\"":p=c.index("\"",p)
+ elif c[p]=="\"":p=c.index("\"",p)
+ elif c[p]=="[":p=c.index("]",p)
  p+=1
